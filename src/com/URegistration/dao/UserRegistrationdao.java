881cb49f7630e97b834ccab305d9f444e325596a
Utkarshthgr8 <70141922+Utkarshthgr8@users.noreply.github.com>
2023-01-16 00:04:45 +0530

    Update UserRegistrationdao.java

--- src/com/URegistration/dao/UserRegistrationdao.java
+++ src/com/URegistration/dao/UserRegistrationdao.java
@@ -5,56 +5,32 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
 public class UserRegistrationdao {
-	
-	
-	
-	String url ="jdbc:mysql://localhost:3306/flyawaydb?characterEncoding=utf8";
-	String username="root";
-	String password="root";
+
+	String url = "jdbc:mysql://localhost:3306/flyawaydb?characterEncoding=utf8";
+	String username = "root";
+	String password = "root";
 	String sql = "INSERT INTO user VALUES (?,?,?);";
-	
-	public boolean check(String name, String email, String pass){
-		boolean flag =false;
-		
+
+	public boolean check(String name, String email, String pass) {
+		boolean flag = false;
+
 		try {
-			
-			
 			Class.forName("com.mysql.jdbc.Driver");
-			
-			Connection con = DriverManager.getConnection(url,username,password);
-			
+			Connection con = DriverManager.getConnection(url, username, password);
 			PreparedStatement st = con.prepareStatement(sql);
-			
-			
-			st.setString(1,name);
-			st.setString(2,email);
-			st.setString(3,pass);
-			
-			
+
+			st.setString(1, name);
+			st.setString(2, email);
+			st.setString(3, pass);
 			int rs = st.executeUpdate();
-			
-			if(rs == 1){
-				flag =true;
+
+			if (rs == 1) {
+				flag = true;
 				return flag;
 			}
-			
-		
-			
-			
 		} catch (Exception e) {
-		
 			e.printStackTrace();
 		}
-		
-		
 		return flag;
-		
-		
-		
-		
-		
 	}
-	
-	
-
 }
