fee7fd652d25352dbd861cb5f0561007e98961cd
Utkarshthgr8 <70141922+Utkarshthgr8@users.noreply.github.com>
2023-01-16 00:02:14 +0530

    Update Logindao.java

--- src/com/URegistration/dao/Logindao.java
+++ src/com/URegistration/dao/Logindao.java
@@ -7,50 +7,30 @@ import java.sql.ResultSet;
 
 public class Logindao {
 
-	String url ="jdbc:mysql://localhost:3306/flyawaydb?characterEncoding=utf8";
-	String username="root";
-	String password="root";
+	String url = "jdbc:mysql://localhost:3306/flyawaydb?characterEncoding=utf8";
+	String username = "root";
+	String password = "root";
 	String sql = "select * from user where Email=? and PassWord=?";
-	
-	public boolean check(String email, String pass){
-		boolean flag =false;
-		
+
+	public boolean check(String email, String pass) {
+		boolean flag = false;
+
 		try {
-			
-			
 			Class.forName("com.mysql.jdbc.Driver");
-			
-			Connection con = DriverManager.getConnection(url,username,password);
-			
+			Connection con = DriverManager.getConnection(url, username, password);
 			PreparedStatement st = con.prepareStatement(sql);
-			
-			
+
 			st.setString(1, email);
 			st.setString(2, pass);
-			
-			
 			ResultSet rs = st.executeQuery();
-			
-			if(rs.next()){
-				flag =true;
+
+			if (rs.next()) {
+				flag = true;
 				return flag;
 			}
-			
-		
-			
-			
 		} catch (Exception e) {
-		
 			e.printStackTrace();
 		}
-		
-		
 		return flag;
-		
-	
-	
-	
+	}
 }
-	
-	
-}	
